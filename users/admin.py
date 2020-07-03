53dd2b58c78c642f0a22b50ae8c0a5d70098f35c
yjw8860 <botoks@naver.com>
2020-07-03 20:29:51 +0900

    update users/admin.py(Using django admin. Using fieldsets)

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
-@admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+@admin.register(models.User) #admin pannel에서 user를 보여줄거다. user를 컨트롤하는 class는 CustomUserAdmin이다
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    fieldsets = (
+        ("BANANA",{'fields':("avatar","gender","bio")}),
+    )
