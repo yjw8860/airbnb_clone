b113b89089d65ea8c3d963abc2e5f0c83de805a1
yjw8860 <botoks723@gmail.com>
2020-07-05 16:22:21 +0900

    #6.0 Room Admin Panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -14,7 +14,24 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition"""
 
-    pass
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = ('instant_book', 'city', 'country')
+
+    #https://docs.djangoproject.com/en/2.2/ref/contrib/admin/ 참고
+    search_fields = ['=city', '^host__username']
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
