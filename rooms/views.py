519970f4ce60540b12d8a4dafa9ba32ca59beb77
yjw8860 <botoks@naver.com>
2020-07-16 16:34:57 +0900

    #11.1 Pagination with Limit and Offset

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from rooms.models import Room
+
+
 def all_rooms(request):
-    all_rooms = Room.objects.all()
+    page = int(request.GET.get('page', 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = Room.objects.all()[offset:limit]
 
     return render(request,
                   template_name='rooms/home.html',
