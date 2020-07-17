84f111a9030170422e9d0ce9aba52caff2a2afda
yjw8860 <botoks@naver.com>
2020-07-17 16:07:25 +0900

    #11.7 Class Based Views

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from rooms import views as room_veiws
 app_name = 'core'
 
 urlpatterns = [
-    path("", room_veiws.all_rooms, name='home')
+    path("", room_veiws.HomeView.as_view(), name='home')
 ]
