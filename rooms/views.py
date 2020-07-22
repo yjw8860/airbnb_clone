86a728f951b6e5d74cabac5099bbc5e186fadf9c
yjw8860 <botoks@naver.com>
2020-07-22 16:40:21 +0900

    #13.1 Starting the Form

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView
 from django.http import Http404
-from rooms.models import Room
+from rooms.models import Room, RoomType
 from django.utils import timezone
 from django.shortcuts import render
+from django_countries import countries
 
 class HomeView(ListView):
 
@@ -29,6 +30,7 @@ def room_detail(request, pk):
         raise Http404()
 
 def search(request):
-    city = request.GET.get('city')
+    city = request.GET.get('city', 'Anywhere')
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city":city})
+    room_types = RoomType.objects.all()
+    return render(request, "rooms/search.html", {"city":city, "countries":countries, 'room_types':room_types})
