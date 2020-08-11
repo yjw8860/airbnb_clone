d5f245c33afba628e0e0123d02443475743ff6fa
yjw8860 <botoks@naver.com>
2020-08-11 17:14:46 +0900

    SearchView 완성!

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 app_name = 'rooms'
-urlpatterns = [path('<int:pk>', views.room_detail, name='detail'),
-               path('search/', views.search, name='search')]
+urlpatterns = [path('<int:pk>', views.room_detail.as_view(), name='detail'),
+               path('search/', views.SearchView.as_view(), name='search')]
--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,10 @@
-from django.views.generic import ListView
-from django.http import Http404
-from rooms.models import Room, RoomType, Amenity, Facility
+from django.views.generic import ListView, DetailView, View
+from django.core.paginator import Paginator
+from rooms.models import Room
 from django.utils import timezone
 from django.shortcuts import render
 from . import forms
-from django_countries import countries
+from . import models
 
 
 class HomeView(ListView):
@@ -23,16 +23,75 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    try:
-        room = Room.objects.get(pk=pk)
-        return render(request, 'rooms/detail.html',
-                      context={'room': room})
-    except Room.DoesNotExist:
-        raise Http404()
+class room_detail(DetailView):
+    """ RoomDetail Definition"""
 
-def search(request):
+    model = models.Room
 
-    form = forms.SearchForm()
+class SearchView(View):
 
-    return render(request, "rooms/search.html", {'form':form})
+    def get(self, request):
+        country = request.GET.get("country")
+
+        if country:
+            form = forms.SearchForm(request.GET)
+            if form.is_valid():
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
+
+                filter_args = {}
+
+                if city != "Anywhere":
+                    filter_args["city__startswith"] = city
+
+                filter_args["country"] = country
+
+                if room_type is not None:
+                    filter_args["room_type"] = room_type
+
+                if price is not None:
+                    filter_args["price__lte"] = price
+
+                if guests is not None:
+                    filter_args["guests__gte"] = guests
+
+                if bedrooms is not None:
+                    filter_args["bedrooms__gte"] = bedrooms
+
+                if beds is not None:
+                    filter_args["beds__gte"] = beds
+
+                if baths is not None:
+                    filter_args["baths__gte"] = baths
+
+                if instant_book is True:
+                    filter_args["instant_book"] = True
+
+                if superhost is True:
+                    filter_args["host__superhost"] = True
+
+                for amenity in amenities:
+                    filter_args["amenities"] = amenity
+
+                for facility in facilities:
+                    filter_args["facilities"] = facility
+
+                qs = models.Room.objects.filter(**filter_args).order_by("-created")
+                paginator = Paginator(qs, 10, orphans=5)
+                page = request.GET.get('page',1)
+                rooms = paginator.get_page(page)
+                return render(request, "rooms/search.html", {'form': form, 'rooms':rooms})
+
+        else:
+            form = forms.SearchForm()
+        return render(request, "rooms/search.html", {'form': form})
