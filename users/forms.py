f2133d9207e4f8f8638054e7542757a6de9253c7
yjw8860 <botoks@naver.com>
2020-08-11 20:13:07 +0900

    users/forms.py error 수정, django 활용해서 users/views.py 쬐끔 간단히 만들기

--- users/forms.py
+++ users/forms.py
@@ -12,7 +12,7 @@ class LoginForm(forms.Form):
 
         try:
             user = models.User.objects.get(username=email)
-            if user.check_password():
+            if user.check_password(password):
                 return self.cleaned_data
             else:
                 self.add_error("password",forms.ValidationError("Password is wrong"))
--- users/views.py
+++ users/views.py
@@ -1,25 +1,24 @@
 from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 # Create your views here.
 
-class LoginView(View):
+class LoginView(FormView):
 
-    def get(self, request):
-        form = forms.LoginForm(initial={"email":"yjw8860@gmail.com"})
-        return render(request, 'users/login.html', {'form':form})
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-        return render(request, 'users/login.html', {"form":form})
+    def form_vaild(self, form):
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
 def log_out(request):
     logout(request)
